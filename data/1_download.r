f0d4f6b21d200d6c76137d75aeb7634e089623d1
sweing <sjweingaertner@gmail.com>
2020-03-16 21:17:36 +0100

    add pixelsize configuration

--- data/1_download.r
+++ data/1_download.r
@@ -6,21 +6,24 @@ source("./base/init.r", chdir=TRUE)
 # ----------------------------------------------
 
 # ----------------------------------------------
-# PREPARATION / LOAD FILE LISTS
+# CONFIG
 # ----------------------------------------------
 validities = c(50, 75)
-pixelSize = 0.1
-rawPath = file.path(folders$tmp, "data", "tropomi")
+pixelSizes = c(0.1)
 
-dir.create(rawPath, showWarnings = FALSE, recursive=TRUE)
-dir.create(h5Path, showWarnings = FALSE, recursive=TRUE)
 t1 = "2019-01-12T00:00:00.000Z"
 t2 = "2019-04-01T00:00:00.000Z"
-removeFile = TRUE
 #t2 = "NOW"
+removeFile = TRUE
+# ----------------------------------------------
 
+# ----------------------------------------------
+# PREPARATION / LOAD FILE LISTS
+# ----------------------------------------------
+rawPath = file.path(folders$tmp, "data", "tropomi")
+dir.create(rawPath, showWarnings = FALSE, recursive=TRUE)
+dir.create(h5Path, showWarnings = FALSE, recursive=TRUE)
 listPath = file.path(rawPath, "list.txt")
-
 
 listUrl = paste0("https://s5phub.copernicus.eu/dhus/api/stub/products?filter=(%20beginPosition:[",
                  t1, "%20TO%20", t2,
@@ -55,10 +58,13 @@ for(i in 1:nrow(getList)){
 
     rawFilePath = file.path(rawPath, identifier)
     
-    h5Path = file.path(folders$tmp, "data", "h5", pixelSize, validities)
-    h5FilePath = file.path(h5Path, paste0(identifier, ".h5"))
+    h5Paths = file.path(folders$tmp, "data", "h5", 
+                        apply(expand.grid(pixelSizes, validities), 1, 
+                              paste, collapse = "/"))
+    h5Paths
+    h5FilePaths = file.path(h5Paths, paste0(identifier, ".h5"))
     
-    if(all(file.exists(h5FilePath), TRUE)) {
+    if(all(file.exists(h5FilePaths), TRUE)) {
         next()
     }
     
@@ -74,31 +80,39 @@ for(i in 1:nrow(getList)){
         print("Downloading completed. Next.")
     }
     
-    pixelNlat = 180*1/pixelSize+1
-    pixelNlon = 360*1/pixelSize+1
-    
-    for(validity in validities){
-        savePath = file.path(folders$tmp, "data", "h5", pixelSize, validity)
-        saveFilePath = file.path(savePath, paste0(identifier, ".h5"))
+    for(pixelSize in pixelSizes){
+        pixelNlat = 180*1/pixelSize+1
+        pixelNlon = 360*1/pixelSize+1
         
-        if(file.exists(saveFilePath)){
-            next()
+        for(validity in validities){
+            savePath = file.path(folders$tmp, "data", "h5", pixelSize, validity)
+            saveFilePath = file.path(savePath, paste0(identifier, ".h5"))
+            
+            if(file.exists(saveFilePath)){
+                next()
+            }
+            print(paste("Creating raster file for validity", validity, "and pixel size", pixelSize))
+            command = paste0("/home/archie/harp/bin/harpconvert --format hdf5 --hdf5-compression 9 -a ",
+                             "'tropospheric_NO2_column_number_density_validity>", 
+                             validity, ";derive(datetime_stop {time}); ",
+                             "bin_spatial(", 
+                             pixelNlat, ",-90,", pixelSize, ",", pixelNlon, ",-180,", pixelSize, 
+                             "); keep(tropospheric_NO2_column_number_density)' ",
+                             rawFilePath, 
+                             " ", 
+                             saveFilePath)
+            
+            print("Converting with harp")
+            
+            system(command, intern = FALSE,
+                   ignore.stdout = FALSE, ignore.stderr = FALSE,
+                   wait = TRUE, input = NULL, 
+                   timeout = 0)
         }
-        print(paste("Creating raster file for validity:", validity))
-        command = paste0("/home/archie/harp/bin/harpconvert --format hdf5 --hdf5-compression 9 -a ",
-                         "'tropospheric_NO2_column_number_density_validity>", validity, ";derive(datetime_stop {time}); ",
-                         "bin_spatial(", "1801,-90,0.1,3601,-180,0.1", "); keep(tropospheric_NO2_column_number_density)' ",
-                         rawFilePath, 
-                         " ", saveFilePath)
         
-        print("Converting with harp")
-        
-        system(command, intern = FALSE,
-               ignore.stdout = FALSE, ignore.stderr = FALSE,
-               wait = TRUE, input = NULL, 
-               timeout = 0)
     }
     
+
     if(removeFile)
         file.remove(rawFilePath)
     
--- data/3_aggregate.r
+++ data/3_aggregate.r
@@ -4,10 +4,20 @@
 rm(list=ls())
 source("./base/init.r", chdir=TRUE)
 # ----------------------------------------------
+
+# ----------------------------------------------
+# SETUP
+# ----------------------------------------------
 year = "2020"
 month = "03"
+minObservations = 7
+# ----------------------------------------------
 
 no2Data = loadData(file.path(folders$tmp, "data", "merged", paste0(month, "_", year, ".rData")))
-mData = no2Data[, .(mValue = median(value)), by = c("X", "Y")]
+
+mData = no2Data[, .(mValue = median(value),
+                    obs = .N), by = c("X", "Y")]
+
+mData = mData[obs > minObservations][, obs := NULL]
 
 saveData(mData, file.path(folders$tmp, "data", "aggregated", paste0(month, "_", year, ".rData")))
